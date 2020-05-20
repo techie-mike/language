397d727a6c82bb6acb45473ef72df67e349f130d
Texnar <kaskovm2014@ya.ru>
2020-05-21 01:54:41 +0300

    Add prototype future function: printf, scanf, exit

--- backend_x86_x64/tests/printf_scanf.asm
+++ backend_x86_x64/tests/printf_scanf.asm
@@ -19,26 +19,49 @@ text	db	"Hello, world!\n"
 
 section .text
 Start:        
+
+;----------------SCANF----------------
+		and esp, ~0xf
+		
+		sub rsp, 32				;	new 32 bytes of shadow space
+		mov rcx, -10
+		call GetStdHandle
+		mov rcx, rax
+		add rsp, 32
+		
 		sub rsp, 8
 		mov rdi, rsp
+		sub rsp, 8
+		mov rsi, rsp
 		
+		sub rsp, 32 + 8 + 8
+		mov dword[rsp+32], 0
+		mov r9,rsi
+		mov r8, 8
+		mov rdx, rdi
+		call ReadConsoleA
+		add rsp, 48
+		add rsp, 8
+		
+;----------------SCANF----------------
+		
+;---------------PRINTF----------------	
 		sub rsp, 32
 		mov rcx, -11
 		call GetStdHandle
-		mov rcx, rax
-		
-		add rsp, 32			;	end of shadow space
-		
-	
-		sub rsp, 32 + 8 + 8			;	new 32 bytes of shadow space 
+		mov rcx, rax		
+		add rsp, 32				;	end of shadow space
+
+		sub rsp, 32 + 8			;	new 32 bytes of shadow space 
 		mov dword [rsp + 32], 0
-		mov r9, 0
-		mov r8, 5
-		lea rdx, [text]
+		mov r9, rsi
+		mov r8, 10
+		mov rdx, rdi
 		call WriteConsoleA
 		
 		add rsp, 48
 		
+;----------------EXIT-----------------
 		xor rcx, rcx
 		call ExitProcess
 	
