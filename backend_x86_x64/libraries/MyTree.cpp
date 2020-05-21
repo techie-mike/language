025886962bc2b7fd3ba34d45a123dd6d12e2e79f
Texnar <kaskovm2014@ya.ru>
2020-05-22 00:13:18 +0300

    Started full refactoring MyTree

---
+++ backend_x86_x64/libraries/MyTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Андрей on 21.05.2020.
+//
+
+#include "MyTree.h"
---
+++ backend_x86_x64/libraries/MyTree.h
@@ -0,0 +1,8 @@
+//
+// Created by Андрей on 21.05.2020.
+//
+
+#ifndef BACKEND_X86_X64_MYTREE_H
+#define BACKEND_X86_X64_MYTREE_H
+
+#endif //BACKEND_X86_X64_MYTREE_H
