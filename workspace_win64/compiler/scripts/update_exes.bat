f9ee32623a7dc01586c673d7f725b17f31e0589b
Mikhail Kaskov <kaskov.ma@phystech.edu>
2020-08-20 22:22:00 +0300

    Removed dependency of applications on .dll from MinGW. Added script to update all .exe files.

---
+++ workspace_win64/compiler/scripts/update_exes.bat
@@ -0,0 +1,16 @@
+echo %WORKSPACE%
+
+:: Update file frontend.exe
+copy ..\..\..\frontend\cmake-build-debug\frontend.exe .\..
+
+:: Update file frontend_back.exe
+copy ..\..\..\frontend_back\cmake-build-debug\frontend_back.exe .\..
+
+:: Update file backend.exe
+copy ..\..\..\backend\cmake-build-debug\backend.exe .\..
+
+:: Update file backend_x86_x64.exe
+copy ..\..\..\backend_x86_x64\cmake-build-debug\backend_x86_x64.exe .\..
+
+:: Update file middleend.exe
+copy ..\..\..\middle_end\cmake-build-debug\middle_end.exe .\..
